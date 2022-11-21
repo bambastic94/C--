baf0fd709d17bfdd5e1fa5c612899352204d4d51
Dmitriy <Wonderful_days@mail.ru>
2022-11-22 00:46:58 +0300

    Добавили восьмую  задачу

---
+++ Eighth/Program.cs
@@ -0,0 +1,28 @@
+﻿// Составить программу, которая выводит числа от 1 до 100, если число кратно 3, то вместо числа пишет FIZZ, если число кратно 5, то пишем BUZZ, если число кратно и 3 и 5, то пишем FIZZBUZZ
+
+int N = 1;
+
+while(N <= 100)
+{
+    if (N % 3 == 0)
+    {
+        if (N % 5 == 0)
+        {
+            System.Console.WriteLine("FIZZBUZZ");
+        }
+        else
+        {
+            System.Console.WriteLine("FIZZ");
+        }
+    }
+    else
+        if (N % 5 == 0)
+        {
+            System.Console.WriteLine("BUZZ");
+        }
+        else
+        {
+            System.Console.WriteLine(N);
+        }
+N++;
+}
