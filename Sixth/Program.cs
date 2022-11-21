103c4fb65f1f510b86e5fb9e310ba2f72b71a077
Dmitriy <Wonderful_days@mail.ru>
2022-11-22 00:54:17 +0300

    Добавили шестую задачу

---
+++ Sixth/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу вычисления модуля числа.
+// 2 -> 2
+// -3 -> 3
+// -7 -> 7
+
+Console.Write("Введите число "); 
+string inputValue = Console.ReadLine(); 
+int value = Convert.ToInt32(inputValue); 
+
+if(value < 0)
+{
+    int minusValue = value * -1;
+    System.Console.WriteLine("Модуль значения " + value + " равен " + minusValue);
+}
+else
+    System.Console.WriteLine("Модуль значения " + value + " равен " + value);
