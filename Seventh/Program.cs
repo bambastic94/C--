f9f72e7aa2ca26da6752d4c457e04c525b6c749d
Dmitriy <Wonderful_days@mail.ru>
2022-11-22 00:19:45 +0300

    Добавили седьмую задачу

---
+++ Seventh/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает значение, а на выходе показывает обратное значение.
+// 1 -> 1
+// 2 -> 0.5
+// 0.25 -> 4
+
+Console.Write("Введите число > "); 
+string inputValue = Console.ReadLine(); 
+double N = Convert.ToDouble(inputValue); 
+
+double value = 1 / N; 
+System.Console.WriteLine("Обратное число " + N + " равен " + value);
