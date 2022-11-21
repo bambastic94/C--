13e776c28cd5e25fef4a5b3f2711459b17131b33
Dmitriy <Wonderful_days@mail.ru>
2022-11-21 23:48:40 +0300

    Сделал четыре программы

---
+++ Fourth/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите программу вычисления функции:
+// x = f(a) функиця куба
+
+Console.Write("Введите число > "); // Вывод приглашения ввода
+string inputValue = Console.ReadLine(); // Ввод строки с терминала (с консоли)
+int value = Convert.ToInt32(inputValue); // Преобразование строки в число
+
+int CubeValue = value * value * value; // вычисление квадрата
+System.Console.WriteLine("Куб числа" + value + " равен " + CubeValue);
