93dfe5c2782acd1a7d8a3afc0b0de1e32f8d5de1
Dmitriy <Wonderful_days@mail.ru>
2022-11-22 00:05:27 +0300

    Добавили пятую задачу

---
+++ Fifth/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Write("Введите число > "); // Вывод приглашения ввода
+string inputValue = Console.ReadLine(); // Ввод строки с терминала (с консоли)
+int value = Convert.ToInt32(inputValue); // Преобразование строки в число
+
+int minusValue = value * -1; // вычисление квадрата
+System.Console.Write("Ряд чисел ");
+while(minusValue <= value)
+{
+    System.Console.Write(minusValue + " ");
+    minusValue++;
+}
+
